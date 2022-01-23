1271aeaf929d90e0fc2a34c378e40b576f71147c
Josue Zolakio <josuezolakiocanal@gmail.com>
2022-01-23 20:24:47 +0100

    Functions lessons completed

--- script.js
+++ script.js
@@ -17,14 +17,14 @@ function logger() {
 
 // Every function can recive parameters
 
-function fruitProcessor(apples, oranges) {
-  return `Juice with ${apples} apples and ${oranges} oranges`;
-}
+// function fruitProcessor(apples, oranges) {
+//   return `Juice with ${apples} apples and ${oranges} oranges`;
+// }
 
 // When we're calling a function we need to type also the Arguments
 
-const mondayJuice = fruitProcessor(54, 61);
-const tuesdayJuice = fruitProcessor(23, 21);
+// const mondayJuice = fruitProcessor(54, 61);
+// const tuesdayJuice = fruitProcessor(23, 21);
 
 // console.log(mondayJuice, tuesdayJuice);
 
@@ -62,9 +62,8 @@ const calcAge1 = function (birthYear) {
   return currentYear - birthYear;
 };
 
-const age1 = calcAge1(1998);
-
-console.log(age1);
+// const age1 = calcAge1(1998);
+// console.log(age1);
 
 /* 
   What's the big diference between Function Declaration 
@@ -74,3 +73,37 @@ console.log(age1);
   or initialized in the code. 
 
 */
+
+/* Third Way Arrow Function 
+
+   Arrow Function is simply a special form of function expression that's 
+   shorter and faster to write. Arrow Function can't use "this." keyword.
+*/
+
+const calcAge2 = birthYear => {
+  const currentYear = new Date().getFullYear();
+  return currentYear - birthYear;
+};
+
+// console.log(calcAge2(2009));
+
+/*
+  Functions calling other functions
+*/
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applesPieces = cutFruitPieces(apples);
+  const orangesPieces = cutFruitPieces(oranges);
+
+  return `Juice with ${applesPieces} apples's Pc and ${orangesPieces} oranges's PC`;
+}
+
+const mondayJuice = fruitProcessor(3, 4);
+const tuesdayJuice = fruitProcessor(12, 16);
+
+// console.log(mondayJuice);
+// console.log(tuesdayJuice);
